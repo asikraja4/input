8ae5fff6a459d57210aa7f8521d71d9a9f36446a
Abdul asik raja <asikraja2001@gmail.com>
2024-12-18 22:42:28 +0530

    Add project files.

---
+++ input/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace input
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.Write("Enter 0 or 1: ");
+            string input = Console.ReadLine();
+            if (input == "1")
+            {
+                Console.WriteLine("True");
+            }
+            else if (input == "0")
+            {
+                Console.WriteLine("False");
+            }
+            else
+            {
+                Console.WriteLine("Invalid input");
+            }
+        }
+    }
+}
